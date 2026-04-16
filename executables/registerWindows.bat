bde2ba87d864f3365fbb8139a2fdebed7fe2f929
CamiCamiCami <sodocamilo@gmail.com>
2026-04-16 20:40:51 -0300

    debug windows

--- executables/registerWindows.bat
+++ executables/registerWindows.bat
@@ -10,8 +10,8 @@ set /p username="Enter your git username: "
 set /p mail="Enter your git mail: "
 set /p token="Enter your login token: "
 
-if not exist %CUSTOM_CONFIG% type null > "%CUSTOM_CONFIG%"
-type null >  "%CUSTOM_TOKEN%"
+if not exist %CUSTOM_CONFIG% type nul > %CUSTOM_CONFIG%
+type nul >  %CUSTOM_TOKEN%
 
 git config --file "%CUSTOM_CONFIG%" user.name "%username%"
 git config --file "%CUSTOM_CONFIG%" user.mail "%mail%"
@@ -19,3 +19,5 @@ git config --file "%CUSTOM_CONFIG%" credential.helper store
 echo https://%username%:%token%@github.com > %CUSTOM_TOKEN%
 
 cd ".."
+
+pause
